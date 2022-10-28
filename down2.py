fbd38dab1aedf4defbb236d45ffb2983cd34b347
Pawel090922 <115668579+Pawel090922@users.noreply.github.com>
2022-10-28 23:14:15 +0200

    Update down2.py

--- down2.py
+++ down2.py
@@ -5,7 +5,7 @@ from function2 import silnia
 class Testfunction(unittest.TestCase):
 
     def test_suma(self):
-        self.assertEqual(silnia(5),120)
+        self.assertEqual(silnia(5),121)
 
 if __name__ == '__main__':
     unittest.main()
