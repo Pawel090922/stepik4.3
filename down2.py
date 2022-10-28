ac64d187c630dc2c55ab77ef6ec5ad1efde7522d
Pawel090922 <115668579+Pawel090922@users.noreply.github.com>
2022-10-28 23:11:02 +0200

    Kolejne dwa pliki

---
+++ down2.py
@@ -0,0 +1,11 @@
+import unittest
+
+from function2 import silnia
+
+class Testfunction(unittest.TestCase):
+
+    def test_suma(self):
+        self.assertEqual(silnia(5),120)
+
+if __name__ == '__main__':
+    unittest.main()
